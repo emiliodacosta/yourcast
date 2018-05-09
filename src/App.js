6c3c3b8dbf1e1e7e69f32fd3a9effd010a86f26d
Emilio da Costa <emidac@gmail.com>
2018-05-09 17:02:48 -0400

    preview working and css centering / text color fixes

--- src/App.js
+++ src/App.js
@@ -12,11 +12,12 @@ class App extends Component {
       weatherData: {},
       lat: 0,
       lon: 0,
-      preview: ''
+      triggerPreview: false,
+      previewCondition: ''
     }
     this.getCoordsAndWeather = this.getCoordsAndWeather.bind(this)
     this.getMusic = this.getMusic.bind(this)
-    // this.onPreviewChange = this.onPreviewChange.bind(this)
+    this.onPreviewChange = this.onPreviewChange.bind(this)
   }
 
   getCoordsAndWeather() {
@@ -34,7 +35,7 @@ class App extends Component {
         weatherData = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=3faeb97e9db97e8a743f5dc0b1e043ef'
         return axios.get(weatherData)
           .then(function (response) {
-            console.log(response)
+            // console.log(response)
             return response.data
           })
           .then(weather => {
@@ -48,28 +49,31 @@ class App extends Component {
   getMusic(condition) {
     switch (condition) {
       case 'Clear':
-        return <ReactPlayer url="https://www.youtube.com/watch?v=sYi7uEvEEmk" playing={false} />
+        return <ReactPlayer url='https://www.youtube.com/watch?v=sYi7uEvEEmk' playing={false} />
       case 'Clouds':
-        return <ReactPlayer url="https://www.youtube.com/watch?v=HPODefkT1Qg" playing={false} />
+        return <ReactPlayer url='https://www.youtube.com/watch?v=HPODefkT1Qg' playing={false} />
       case 'Drizzle':
-        return <ReactPlayer url="https://www.youtube.com/watch?v=4fSS36Xve4k" playing={false} />
+        return <ReactPlayer url='https://www.youtube.com/watch?v=4fSS36Xve4k' playing={false} />
       case 'Rain':
-        return <ReactPlayer url="https://www.youtube.com/watch?v=46N0PgjFqyw" playing={false} />
+        return <ReactPlayer url='https://www.youtube.com/watch?v=46N0PgjFqyw' playing={false} />
       case 'Thunderstorm':
-        return <ReactPlayer url="https://soundcloud.com/deuxhelix/ambulance-deuxhelixrmx" playing={false} />
+        return <ReactPlayer url='https://soundcloud.com/deuxhelix/ambulance-deuxhelixrmx' playing={false} />
       case 'Snow':
-        return <ReactPlayer url="https://soundcloud.com/ibanzero/voltereta" playing={false} />
+        return <ReactPlayer url='https://soundcloud.com/ibanzero/voltereta' playing={false} />
       case 'Atmosphere':
-        return <ReactPlayer url="https://www.youtube.com/watch?v=hEm0zbJe0jY" playing={false} />
+        return <ReactPlayer url='https://www.youtube.com/watch?v=hEm0zbJe0jY' playing={false} />
       default:
     }
   }
 
-  // onPreviewChange(condition, evt) {
-  //   condition = evt.target.value
-  //   // have div below in render function, showPreview boolean dependent on selection not being select or same as actual weather
-  //   return <div>{this.getWeatherConditions(condition, condition)}</div>
-  // }
+  onPreviewChange(evt) {
+    let condition = evt.target.value
+    this.setState({
+      triggerPreview: true,
+      previewCondition: condition
+    })
+    // have div below in render function, showPreview boolean dependent on selection not being select or same as actual weather
+  }
 
   componentDidMount() {
     this.getCoordsAndWeather()
@@ -80,6 +84,8 @@ class App extends Component {
     const lon = this.state.lon
     const weather = this.state.weatherData
     const getMusic = this.getMusic
+    const triggerPreview = this.state.triggerPreview
+    const previewCondition = this.state.previewCondition
     let now = new Date()
     console.log(weather)
     return (
@@ -98,19 +104,50 @@ class App extends Component {
             }
           </div>
         </header>
-        <div className='map'>
+        <div className="map">
           {lat !== 0 && lon !== 0 ?
-            <img src={"https://maps.googleapis.com/maps/api/staticmap?center=" + lat + ",+" + lon + "&zoom=13&scale=1&size=600x300&maptype=roadmap&key=AIzaSyD_PJaoTDrgpGawta73DrtuopubfAwj0L8&format=png&visual_refresh=true"} alt="Google Map" />
+            <img src={'https://maps.googleapis.com/maps/api/staticmap?center=' + lat + ',+' + lon + '&zoom=13&scale=1&size=600x300&maptype=roadmap&key=AIzaSyD_PJaoTDrgpGawta73DrtuopubfAwj0L8&format=png&visual_refresh=true'} alt="Google Map" />
             : <p> loading map... </p>
           }
         </div>
-
-        <div>
+        <br />
+        <div className="weatherTrackTitle">
+            {weather.main && weather.weather[0].main ?
+              <p>Music for {weather.weather[0].main}</p>
+              : null
+            }
+        </div>
+        <div className="weatherTrack">
           {weather.main && weather.weather[0].main ?
             getMusic(weather.weather[0].main)
             : <p> loading music... </p>
           }
         </div>
+        <br />
+        <br />
+        <div className="previewSelect">
+          <span>Preview music for other weather conditions: </span>
+          <select selected="select" onChange={(evt) => this.onPreviewChange(evt)}>
+            <option value="select">select</option>
+            <option value="Clear">Clear</option>
+            <option value="Clouds">Clouds</option>
+            <option value="Drizzle">Drizzle</option>
+            <option value="Rain">Rain</option>
+            <option value="Thunderstorm">Thunderstorm</option>
+            <option value="Snow">Snow</option>
+            <option value="Atmosphere">Mist</option>
+          </select>
+        </div>
+        <br />
+        <br />
+        <div className="previewTrack">
+          {triggerPreview ?
+            getMusic(previewCondition)
+          : null
+          }
+        </div>
+        <br />
+        <br />
       </div>
     )
   }
@@ -119,18 +156,3 @@ class App extends Component {
 export default App
 
 // For Celsius {(weather.main.temp - 273.15).toFixed(1)}°C&nbsp;
-
-
-// <div className='preview'>
-// <span>Preview music for other weather conditions</span>
-// <select selected="select" onChange={(evt) => this.onPreviewChange(condition, evt)}>
-//   <option value="select">select</option>
-//   <option value="clear sky">clear sky</option>
-//   <option value="clouds">clouds</option>
-//   <option value="rain">rain</option>
-//   <option value="thunderstorm">thunderstorm</option>
-//   <option value="snow">snow</option>
-//   <option value="mist">mist</option>
-// </select>
-// </div>
-
